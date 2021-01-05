abdc652ffba24e79d873255e2a652d16e98f2a57
dallanoce <francesco.dallanoce@mail.polimi.it>
2021-01-05 17:11:27 +0100

    05 January first update

--- scraping.py
+++ scraping.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import numpy as np
 
 
 def scraping(link):
@@ -90,3 +91,36 @@ def scrapingCategory(link):
     print(data)
 
     return data
+
+def scrapingSomministrationPoints(link): #WIP
+    driver = webdriver.Chrome(executable_path=r'C:\WebDrivers\chromedriver.exe')
+    driver.get(link)
+
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "label")))
+
+    scroll = driver.find_elements_by_css_selector('html body div#pbiAppPlaceHolder root div div#embedWrapperID.embedWrapper div.landingController.fillAvailableSpace div.landingContainer div#reportLandingContainer.embeddedLandingRootContent.reportContainerContent.isActive.embeddedPV.embeddedLandingRootContentLogoVisible div.embeddedPV exploration-container exploration-container-modern div.explorationContainer.explorationWithChrome.themeableElement.reading.newPaneColors div.explorationContent.fill div.explorationHost.explorationHostNoAppBar exploration-host#pvExplorationHost div.fillAvailableSpace.verticalItemsContainer div.horizontalItemsContainer exploration.exploreCanvasToBottom div.exploration explore-canvas-modern.explore-canvas-modern-style div.exploreCanvas.themeableElement.disableAnimations.stylableVisualContainerHeader.master-form-factor div.canvasFlexBox div.displayAreaContainer.droppableElement.ui-droppable div.displayArea.disableAnimations.actualSizeAlignCenter.actualSizeAlignTop.actualSizeOrigin div.visualContainerHost visual-container-repeat visual-container-modern.visual-container-component transform.bringToFront div.visualContainer.unselectable.readMode.hideBorder.noVisualTitle.visualHeaderAbove.droppableElement.ui-droppable div div.vcBody.themableBackgroundColor.themableBorderColorSolid visual-modern div.visual.visual-tableEx.allow-deferred-rendering div.tableExContainer div.tableEx div.scroll-bar-div')
+    driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', scroll)
+    print(scroll)
+
+    parent = driver.find_elements_by_css_selector(
+        "html body div#pbiAppPlaceHolder root div div#embedWrapperID.embedWrapper div.landingController.fillAvailableSpace div.landingContainer div#reportLandingContainer.embeddedLandingRootContent.reportContainerContent.isActive.embeddedPV.embeddedLandingRootContentLogoVisible div.embeddedPV exploration-container exploration-container-modern div.explorationContainer.explorationWithChrome.themeableElement.reading.newPaneColors div.explorationContent.fill div.explorationHost.explorationHostNoAppBar exploration-host#pvExplorationHost div.fillAvailableSpace.verticalItemsContainer div.horizontalItemsContainer exploration.exploreCanvasToBottom div.exploration explore-canvas-modern.explore-canvas-modern-style div.exploreCanvas.themeableElement.disableAnimations.stylableVisualContainerHeader.master-form-factor div.canvasFlexBox div.displayAreaContainer.droppableElement.ui-droppable div.displayArea.disableAnimations.actualSizeAlignCenter.actualSizeAlignTop.actualSizeOrigin div.visualContainerHost visual-container-repeat visual-container-modern.visual-container-component transform.bringToFront div.visualContainer.unselectable.readMode.hideBorder.noVisualTitle.visualHeaderAbove.droppableElement.ui-droppable div div.vcBody.themableBackgroundColor.themableBorderColorSolid visual-modern div.visual.visual-tableEx.allow-deferred-rendering div.tableExContainer div.tableEx div.innerContainer div.bodyCells")
+    print(parent)
+
+
+
+
+    children = parent[0].find_elements_by_xpath('.//*')
+
+    #print(children)
+
+    data = [child.get_attribute("title") for child in children]
+    data.insert(0, "title")
+
+    print(data)
+
+    return data
+
+
+#scrapingSomministrationPoints( "https://app.powerbi.com/view?r=eyJrIjoiMzg4YmI5NDQtZDM5ZC00ZTIyLTgxN2MtOTBkMWM4MTUyYTg0IiwidCI6ImFmZDBhNzVjLTg2NzEtNGNjZS05MDYxLTJjYTBkOTJlNDIyZiIsImMiOjh9")
