079c8bf3ef4e05eebff9631408c0460af402f8fd
dallanoce <francesco.dallanoce@mail.polimi.it>
2021-01-06 15:06:12 +0100

    06 january, first update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import conversione_csv
 import regions_elaboration
 import plot_regions
 
-DATE = '05_01'
+DATE = '06_01'
 
 INPUT_PATH = 'E:/vaccinazione_COVID19/andamento_giornaliero_immagine/'
 DEST_PATH = 'E:/vaccinazione_COVID19/andamento_giornaliero/'
