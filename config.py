7bf320acc11105106dbe7a1c1a5ab7ecf43767bc
dallanoce <francesco.dallanoce@mail.polimi.it>
2021-01-06 20:16:24 +0100

    Reformat main variable: now in config.py New vaccine available from pfizer, more or less 215k

---
+++ config.py
@@ -0,0 +1,13 @@
+
+DATE = '06_01'
+
+INPUT_PATH = 'E:/vaccinazione_COVID19/andamento_giornaliero_immagine/'
+DEST_PATH = 'E:/vaccinazione_COVID19/andamento_giornaliero/'
+DEST_PATH_GROUP = 'E:/vaccinazione_COVID19/andamento_giornaliero_gruppi/'
+DEST_PATH_CATEGORY = 'E:/vaccinazione_COVID19/andamento_giornaliero_categorie/'
+
+PATH = 'E:/vaccinazione_COVID19/andamento_giornaliero/'
+DEST_PATH_REGIONS = 'E:/vaccinazione_COVID19/andamento_giornaliero_regioni/'
+DEST_PATH_GRAFICI = 'E:/vaccinazione_COVID19/andamento_giornaliero_regioni_grafici/'
+
+LINK = "https://app.powerbi.com/view?r=eyJrIjoiMzg4YmI5NDQtZDM5ZC00ZTIyLTgxN2MtOTBkMWM4MTUyYTg0IiwidCI6ImFmZDBhNzVjLTg2NzEtNGNjZS05MDYxLTJjYTBkOTJlNDIyZiIsImMiOjh9"
--- main.py
+++ main.py
@@ -1,36 +1,24 @@
 import conversione_csv
 import regions_elaboration
 import plot_regions
-
-DATE = '06_01'
-
-INPUT_PATH = 'E:/vaccinazione_COVID19/andamento_giornaliero_immagine/'
-DEST_PATH = 'E:/vaccinazione_COVID19/andamento_giornaliero/'
-DEST_PATH_GROUP = 'E:/vaccinazione_COVID19/andamento_giornaliero_gruppi/'
-DEST_PATH_CATEGORY = 'E:/vaccinazione_COVID19/andamento_giornaliero_categorie/'
-
-PATH = 'E:/vaccinazione_COVID19/andamento_giornaliero/'
-DEST_PATH_REGIONS = 'E:/vaccinazione_COVID19/andamento_giornaliero_regioni/'
-DEST_PATH_GRAFICI = 'E:/vaccinazione_COVID19/andamento_giornaliero_regioni_grafici/'
-
-LINK = "https://app.powerbi.com/view?r=eyJrIjoiMzg4YmI5NDQtZDM5ZC00ZTIyLTgxN2MtOTBkMWM4MTUyYTg0IiwidCI6ImFmZDBhNzVjLTg2NzEtNGNjZS05MDYxLTJjYTBkOTJlNDIyZiIsImMiOjh9"
+import config
 
 
 def main():
     print("CSV loading...")
-    conversione_csv.convertion(DATE, LINK, DEST_PATH, latest=True)
+    conversione_csv.convertion(config.DATE, config.LINK, config.DEST_PATH, latest=True)
 
-    conversione_csv.convertionGroup(DATE, LINK, DEST_PATH_GROUP, latest=True)
+    conversione_csv.convertionGroup(config.DATE, config.LINK, config.DEST_PATH_GROUP, latest=True)
 
-    conversione_csv.convertionCategory(DATE, LINK, DEST_PATH_CATEGORY, latest=True)
+    conversione_csv.convertionCategory(config.DATE, config.LINK, config.DEST_PATH_CATEGORY, latest=True)
     print("Done.")
 
     print("Regions creation..")
-    regions_elaboration.regionsElaborations(PATH, DEST_PATH_REGIONS)
+    regions_elaboration.regionsElaborations(config.PATH, config.DEST_PATH_REGIONS)
     print("Done.")
 
     print("Plotting regions..")
-    plot_regions.plotRegions(DEST_PATH_REGIONS, DEST_PATH_GRAFICI)
+    plot_regions.plotRegions(config.DEST_PATH_REGIONS, config.DEST_PATH_GRAFICI)
     print("Done.")
 
 
