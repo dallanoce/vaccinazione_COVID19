ebeda7166d2743980028d74176f12e8391e82fa4
dallanoce <francesco.dallanoce@mail.polimi.it>
2021-01-08 21:11:02 +0100

    Update plot: now display also daily vaccinations, bar plot

--- plot_regions.py
+++ plot_regions.py
@@ -18,14 +18,22 @@ def plotRegions(path, dest_path):
 
         date = df['Data'].copy()
 
+        daily = []
+
+        for j in range(1,len(df['Somministrazioni'])):
+            daily.append(df.at[j,'Somministrazioni'] - df.at[j-1,'Somministrazioni'])
+        print(daily)
+        daily.insert(0,df.at[0,'Somministrazioni'])
+
         for k in range(0,len(date)):
             date[x] = date[k].strip("2020-")
 
         ax = df.plot(x='Data', y=['Somministrazioni', 'Dosi Consegnate'], kind='line', title=filename, grid=True,
                 xticks=np.arange(len(df['Data'])),  lw = 2, rot = 90,
-                legend=True, style='.-')
-        ax.set_xlabel("Data")
+                 style='.-')
+        ax.set_xlabel("Data (YYYY-MM-DD)")
         ax.set_ylabel("Dosi")
+        plt.xlim(0,len(df['Data']))
 
         for i, txt in enumerate(df['Somministrazioni']):
             plt.text(i - 0.3, txt, txt)
@@ -33,7 +41,13 @@ def plotRegions(path, dest_path):
         for i, txt in enumerate(df['Dosi Consegnate']):
             plt.text(i - 0.3, txt, txt)
 
-        plt.fill_between(np.arange(len(df['Data (YYYY-MM-DD)'])), df['Somministrazioni'], alpha=0.4)
+        for i, txt in enumerate(daily):
+            plt.text(i - 0.22, txt, txt,color='#3b4d8a')
+
+        plt.fill_between(np.arange(len(df['Data'])), df['Somministrazioni'], alpha=0.4,label='_nolegend_')
+
+        plt.bar(np.arange(len(df['Data'])),daily,width=0.55,color = '#3b4d8a')
+        plt.legend(['Somministrazioni', 'Dosi Consegnate','Somministrazioni giornaliere'])
 
         plt.savefig(fname=dest_path + filename + '.png',pad_inches = 0.3,bbox_inches= 'tight')
         #plt.show()
@@ -41,9 +55,9 @@ def plotRegions(path, dest_path):
         print(x)
 
 
-"""
+
 DEST_PATH_REGIONS = 'E:/vaccinazione_COVID19/andamento_giornaliero_regioni/'
 DEST_PATH_GRAFICI = 'E:/vaccinazione_COVID19/andamento_giornaliero_regioni_grafici/'
 
 plotRegions(DEST_PATH_REGIONS, DEST_PATH_GRAFICI)
-"""
+
